d6e5b3b73ce1fd62cf96f68863ce5bf7a4f7bf29
ellisa1419 <ellisa.khoja@gmail.com>
2018-10-23 12:19:46 -0400

    Fixed default redirection to Homepage

--- src/com/albany/foodOnWheels/controller/HomePage.java
+++ src/com/albany/foodOnWheels/controller/HomePage.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HomePage
  */
-@WebServlet("/HomePage")
+@WebServlet({"/HomePage","/"})
 public class HomePage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
