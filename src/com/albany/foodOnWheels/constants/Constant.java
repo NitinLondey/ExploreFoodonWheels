e300b82262e028745155647bd35e49428dbfac74
Ellisa Khoja <ellisa.khoja@gmail.com>
2018-12-19 22:56:26 -0500

    Update Constant.java

--- src/com/albany/foodOnWheels/constants/Constant.java
+++ src/com/albany/foodOnWheels/constants/Constant.java
@@ -1,6 +1,6 @@
 package com.albany.foodOnWheels.constants;
 
 public interface Constant {
-String api_key= "J5z3W9B3k0wWIltu0tvx8jZgyImZfAECNNxe0JZ2iSL1Ul9PqEaT1ooEXClSWQia";
+String api_key= "";
 String file_upload = "C:\\Users\\nitin londey\\Desktop\\SE\\New folder\\10th_Novemeber_2018\\ExploreFoodonWheels\\WebContent\\uploads"; // change this
 }
